9f615fb9ef129df500b4a2831474ca8c3bf18ecb
jamiegl <30474768+jamiegl@users.noreply.github.com>
2021-04-06 15:31:34 +0100

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+set SPACEFISH_PROMPT_ORDER time user host dir git package node docker ruby golang php rust haskell julia aws conda pyenv kubecontext exec_time line_sep battery jobs exit_code char
+set SPACEFISH_CHAR_SYMBOL '>'
+set SPACEFISH_USER_SHOW always
+set SPACEFISH_HOST_SHOW always
+set SPACEFISH_DIR_COLOR 'd787ff'
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/jamie/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
